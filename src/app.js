fbcf6240fbad28f88dc6d0ad13c61aa3037a3789
Mike Torra <mtorra@salesforce.com>
2017-04-19 17:12:59 -0400

    refactor before adding support for cloudwatch events

--- src/app.js
+++ src/app.js
@@ -17,29 +17,37 @@ function _gen_msg_list_key(subj) {
   return `messages:${subj}`;
 }
 
-function _handler(event, context, callback) {
-  let sns_obj = event.Records[0].Sns;
-  let lock_key = _gen_lock_key(sns_obj.Subject);
-  return rc.setAsync(lock_key, 1, 'NX', 'EX', DEBOUNCE_TIME_S)
+function _debounce_sns(subject, message, target_topic_arn, debounce_time_s, callback) {
+  let lock_key = _gen_lock_key(subject);
+  return rc.setAsync(lock_key, 1, 'NX', 'EX', debounce_time_s)
   .then(function(result){
-    let msg_list_key = _gen_msg_list_key(sns_obj.Subject);
+    let msg_list_key = _gen_msg_list_key(subject);
     if(result === 'OK') {
       return rc.multi()
         .lrange(msg_list_key, 0, -1)
         .del(msg_list_key)
         .exec(function(err, replies){
-          let message = [sns_obj.Message].concat(replies[0]).join('\n');
+          let aggregated_message = [message].concat(replies[0]).join('\n');
           SNS.publish({
-            Message: message,
-            Subject: sns_obj.Subject,
-            TopicArn: process.env.TARGET_SNS_TOPIC_ARN
+            Message: aggregated_message,
+            Subject: subject,
+            TopicArn: target_topic_arn
           }, callback);
         });
     } else {
-      rc.rpush(msg_list_key, sns_obj.Message);
+      rc.rpush(msg_list_key, message);
       callback();
     }
   }).catch(callback);
+}
+
+function _handler(event, context, callback) {
+  if(event.Records && event.Records[0] && event.Records[0].Sns) {
+    let sns_obj = event.Records[0].Sns;
+    _debounce_sns(sns_obj.Subject, sns_obj.Message, process.env.TARGET_SNS_TOPIC_ARN, DEBOUNCE_TIME_S, callback);
+  } else {
+
+  }
 }
 
 exports.handler = _handler;
--- tst/app.test.js
+++ tst/app.test.js
@@ -36,67 +36,69 @@ describe('handle', function(){
     sandbox.restore();
   });
 
-  it('should forward the first sns message to the real alarm topic', function(done){
-    let fake_sns_event = {
-      Records: [
-        {
-          Sns: {
-            Subject: 'test_subj',
-            Message: 'test msg'
+  describe('sns events', function(){
+    it('should forward the first sns message to the real alarm topic', function(done){
+      let fake_sns_event = {
+        Records: [
+          {
+            Sns: {
+              Subject: 'test_subj',
+              Message: 'test msg'
+            }
           }
-        }
-      ]
-    }
-    handler(fake_sns_event, {}, function(err){
-      expect(publish_spy.args[0][0]).to.eql({
-        Message: 'test msg',
-        Subject: 'test_subj',
-        TopicArn: 'mt_test_arn'
+        ]
+      }
+      handler(fake_sns_event, {}, function(err){
+        expect(publish_spy.args[0][0]).to.eql({
+          Message: 'test msg',
+          Subject: 'test_subj',
+          TopicArn: 'mt_test_arn'
+        });
+        done(err);
       });
-      done(err);
     });
-  });
 
-  it('should debounce messages and send a single digest message', function(){
-    // set the lock key to prevent messages from firing
-    return rc.setAsync('lock:test_subj', 1)
-    .then(function(){
-      let fake_sns_event1 = {
-        Records: [
-          {
-            Sns: {
-              Subject: 'test_subj',
-              Message: 'test msg 1'
+    it('should debounce messages and send a single digest message', function(){
+      // set the lock key to prevent messages from firing
+      return rc.setAsync('lock:test_subj', 1)
+      .then(function(){
+        let fake_sns_event1 = {
+          Records: [
+            {
+              Sns: {
+                Subject: 'test_subj',
+                Message: 'test msg 1'
+              }
             }
-          }
-        ]
-      };
-      return handlerAsync(fake_sns_event1, {});
-    }).then(function(){
-      // delete lock key, so aggregated digest message will fire
-      return rc.delAsync('lock:test_subj');
-    }).then(function(){
-      let fake_sns_event2 = {
-        Records: [
-          {
-            Sns: {
-              Subject: 'test_subj',
-              Message: 'test msg 2'
+          ]
+        };
+        return handlerAsync(fake_sns_event1, {});
+      }).then(function(){
+        // delete lock key, so aggregated digest message will fire
+        return rc.delAsync('lock:test_subj');
+      }).then(function(){
+        let fake_sns_event2 = {
+          Records: [
+            {
+              Sns: {
+                Subject: 'test_subj',
+                Message: 'test msg 2'
+              }
             }
-          }
-        ]
-      };
-      return handlerAsync(fake_sns_event2, {});
-    }).then(function(){
-      expect(publish_spy.calledOnce).to.be.true;
-      expect(publish_spy.args[0][0]).to.eql({
-        Message: ['test msg 2', 'test msg 1'].join('\n'),
-        Subject: 'test_subj',
-        TopicArn: 'mt_test_arn'
+          ]
+        };
+        return handlerAsync(fake_sns_event2, {});
+      }).then(function(){
+        expect(publish_spy.calledOnce).to.be.true;
+        expect(publish_spy.args[0][0]).to.eql({
+          Message: ['test msg 2', 'test msg 1'].join('\n'),
+          Subject: 'test_subj',
+          TopicArn: 'mt_test_arn'
+        });
+        return rc.lrangeAsync('messages:test_subj', 0, -1);
+      }).then(function(messages){
+        expect(messages).to.have.length(0);
       });
-      return rc.lrangeAsync('messages:test_subj', 0, -1);
-    }).then(function(messages){
-      expect(messages).to.have.length(0);
     });
   });
 
