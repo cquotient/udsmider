a0c0268830243402ad81774013d0f080711e3dc6
Mike Torra <mtorra@salesforce.com>
2017-05-25 13:51:55 -0400

    declare redis client in handler function?

--- src/app.js
+++ src/app.js
@@ -5,8 +5,6 @@ const AWS = require('aws-sdk');
 const SNS = BB.promisifyAll(new AWS.SNS({apiVersion: '2010-03-31'}));
 const redis = require('redis');
 
-let rc;
-
 const DEBOUNCE_TIME_S = 300; //5 minutes per message forwarded
 
 function _gen_lock_key(subj) {
@@ -17,7 +15,7 @@ function _gen_msg_list_key(subj) {
   return `messages:${subj}`;
 }
 
-function _send_aggr_alarm(subject, curr_msgs, target_topic_arn) {
+function _send_aggr_alarm(rc, subject, curr_msgs, target_topic_arn) {
   console.log(`sending aggregated alarm for '${subject}' to ${target_topic_arn}`);
   let msg_list_key = _gen_msg_list_key(subject);
   let multi = BB.promisifyAll(rc.multi());
@@ -39,12 +37,12 @@ function _send_aggr_alarm(subject, curr_msgs, target_topic_arn) {
     });
 }
 
-function _debounce_sns(subject, message, target_topic_arn, debounce_time_s) {
+function _debounce_sns(rc, subject, message, target_topic_arn, debounce_time_s) {
   let lock_key = _gen_lock_key(subject);
   return rc.setAsync(lock_key, 1, 'NX', 'EX', debounce_time_s)
   .then(function(result){
     if(result === 'OK') {
-      return _send_aggr_alarm(subject, [message], target_topic_arn);
+      return _send_aggr_alarm(rc, subject, [message], target_topic_arn);
     } else {
       console.log(`debounced: '${message}' with subject: '${subject}'`)
       return rc.rpushAsync(_gen_msg_list_key(subject), message);
@@ -52,7 +50,7 @@ function _debounce_sns(subject, message, target_topic_arn, debounce_time_s) {
   });
 }
 
-function _check_leftovers(target_topic_arn) {
+function _check_leftovers(rc, target_topic_arn) {
   /*
     i know what you're thinking, this seems kind of crazy. keep in mind that there
     should pretty much always be very few keys in this redis instance, and it should
@@ -79,32 +77,32 @@ function _check_leftovers(target_topic_arn) {
       return acc;
     }, []);
     console.log(`got leftovers: ${JSON.stringify(leftovers)}`);
-    return BB.all(leftovers.map((leftover_subj) => _send_aggr_alarm(leftover_subj, [], target_topic_arn)));
+    return BB.all(leftovers.map((leftover_subj) => _send_aggr_alarm(rc, leftover_subj, [], target_topic_arn)));
   })
 }
 
-function _cleanup(err, cb) {
+function _cleanup(err, rc, cb) {
   rc.end(true);
   cb(err);
 }
 
 function _handler(event, context, callback) {
-  rc = BB.promisifyAll(redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST));
+  let rc = BB.promisifyAll(redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST));
   console.log(`debouncing with: ${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`)
   if(event.Records && event.Records[0] && event.Records[0].Sns) {
     let sns_obj = event.Records[0].Sns;
     console.log(`handling sns: ${JSON.stringify(sns_obj)}`);
-    _debounce_sns(sns_obj.Subject, sns_obj.Message, process.env.TARGET_SNS_TOPIC_ARN, DEBOUNCE_TIME_S).then((function(){
-      _cleanup(null, callback);
+    _debounce_sns(rc, sns_obj.Subject, sns_obj.Message, process.env.TARGET_SNS_TOPIC_ARN, DEBOUNCE_TIME_S).then((function(){
+      _cleanup(null, rc, callback);
     })).catch(function(err){
-      _cleanup(err, callback);
+      _cleanup(err, rc, callback);
     });
   } else {
     console.log(`handling cloudwatch event`);
-    _check_leftovers(process.env.TARGET_SNS_TOPIC_ARN).then(function(){
-      _cleanup(null, callback);
+    _check_leftovers(rc, process.env.TARGET_SNS_TOPIC_ARN).then(function(){
+      _cleanup(null, rc, callback);
     }).catch(function(err){
-      _cleanup(err, callback);
+      _cleanup(err, rc, callback);
     });
   }
 }
