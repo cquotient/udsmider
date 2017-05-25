220140b05a28ccecb2fb40e98b1f28331aa8fbbf
Mike Torra <mtorra@salesforce.com>
2017-05-25 13:49:40 -0400

    not fully working in lambda, but this seems like it has all the error handling and callbacks handled...

--- src/app.js
+++ src/app.js
@@ -18,17 +18,23 @@ function _gen_msg_list_key(subj) {
 }
 
 function _send_aggr_alarm(subject, curr_msgs, target_topic_arn) {
+  console.log(`sending aggregated alarm for '${subject}' to ${target_topic_arn}`);
   let msg_list_key = _gen_msg_list_key(subject);
   let multi = BB.promisifyAll(rc.multi());
   multi.lrangeAsync(msg_list_key, 0, -1);
   multi.delAsync(msg_list_key);
   return multi.execAsync()
     .then(function(replies){
+      console.log(`got redis response: ${JSON.stringify(replies)}`);
       let aggregated_message = curr_msgs.concat(replies[0]).join('\n');
       return SNS.publishAsync({
         Message: aggregated_message,
         Subject: subject,
         TopicArn: target_topic_arn
+      }).then(function(resp){
+        console.log(`sns response: ${JSON.stringify(resp)}`);
+      }).catch(function(err){
+        console.log(`sns error: ${JSON.stringify(err)}`);
       });
     });
 }
@@ -40,6 +46,7 @@ function _debounce_sns(subject, message, target_topic_arn, debounce_time_s) {
     if(result === 'OK') {
       return _send_aggr_alarm(subject, [message], target_topic_arn);
     } else {
+      console.log(`debounced: '${message}' with subject: '${subject}'`)
       return rc.rpushAsync(_gen_msg_list_key(subject), message);
     }
   });
@@ -71,26 +78,34 @@ function _check_leftovers(target_topic_arn) {
       }
       return acc;
     }, []);
+    console.log(`got leftovers: ${JSON.stringify(leftovers)}`);
     return BB.all(leftovers.map((leftover_subj) => _send_aggr_alarm(leftover_subj, [], target_topic_arn)));
   })
 }
 
-function _cleanup(cb) {
+function _cleanup(err, cb) {
   rc.end(true);
-  cb();
+  cb(err);
 }
 
 function _handler(event, context, callback) {
   rc = BB.promisifyAll(redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST));
+  console.log(`debouncing with: ${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`)
   if(event.Records && event.Records[0] && event.Records[0].Sns) {
     let sns_obj = event.Records[0].Sns;
+    console.log(`handling sns: ${JSON.stringify(sns_obj)}`);
     _debounce_sns(sns_obj.Subject, sns_obj.Message, process.env.TARGET_SNS_TOPIC_ARN, DEBOUNCE_TIME_S).then((function(){
-      _cleanup(callback);
-    })).catch(callback);
+      _cleanup(null, callback);
+    })).catch(function(err){
+      _cleanup(err, callback);
+    });
   } else {
+    console.log(`handling cloudwatch event`);
     _check_leftovers(process.env.TARGET_SNS_TOPIC_ARN).then(function(){
-      _cleanup(callback);
-    }).catch(callback);
+      _cleanup(null, callback);
+    }).catch(function(err){
+      _cleanup(err, callback);
+    });
   }
 }
 
